f50140a5f4ba74d1c2c136af19072540d81d6e55
krzysztoffila <umowy.fila@gmail.com>
2023-03-13 16:26:59 +0100

    V1

--- main.js
+++ main.js
@@ -5,13 +5,11 @@ const names = ["Jagienka", "Dobrawa (Dobrusia)", "Daria", "Asia", "Kira", "Marys
 
 const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę", "Szczerze uważam", "Na 100% twierdzę", "Jestem pewien"];
 
-// console.log(names[index]);
-
 const nameGenerator = () => {
- const indexName = Math.floor(Math.random() * names.length);
- // const indexPrefix = Math.floor(Math.random() * prefixs.length);
- // div.textContent = `${prefixs[indexPrefix]}, że najlepsze będzie imię ${names[indexName]}`
- div.textContent = `${prefixs[Math.floor(Math.random() * prefixs.length)]}, że najlepsze będzie imię ${names[indexName]}`
+    const index = Math.floor(Math.random() * names.length);
+    const indexPrefix = Math.floor(Math.random() * prefixs.length);
+
+    div.textContent = `${prefixs[indexPrefix]}, ze najlepsze bedzię imię ${names[index]}`
 }
 
 btn.addEventListener('click', nameGenerator)
